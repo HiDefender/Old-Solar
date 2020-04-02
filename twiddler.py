86e501b4ed3d2d063a119704a971256c78d18318
HiDefender <24965421+HiDefender@users.noreply.github.com>
2020-04-02 18:28:03 -0500

    Performance Improvements

--- twiddler.py
+++ twiddler.py
@@ -1,6 +1,6 @@
 from z3 import *
+from datetime import datetime, timedelta
 
-from datetime import datetime
 setupTime = datetime.now()
 s = Solver()
 
@@ -14,16 +14,20 @@ s = Solver()
 #   range for the highest CPS it can find. It will quit once the difference between the
 #   highest satisfiable (sat) solution and the lowest unsatisfiable/timeout (unsat/unknown)
 #   is less than the resolution.
+print("12 buttons limited to 15 chars expanded form.")
 cps_hi = 5.0
 cps_lo = 1.0
-cps_res = 0.05
+cps_res = 0.01
 # The number of miliseconds the solver should spend on any single iteration.
 #   Higher is better and slower.
-timeout = 600 * 1000
+timeout = timedelta(minutes=15)
 # Ignores all n_grams (except single alphabet characters) with a frequency below the cutoff.
 #   Lower is better and slower.
 cutoff = 8000000
 
+print(f'Hi: {cps_hi}, Lo: {cps_lo}, Resolution: {cps_res}')
+print(f'Timeout: {timeout}, Cutoff: {cutoff}')
+print("---------------------------------------")
 
 # ***************************************
 # Load and modify ngram data
@@ -111,7 +115,35 @@ for i in range(26, len(n_gram)):
 
 
 # **********************************************
-# Soft constraints
+# Time Saving Heuristics
+#  - Help the solver by adding constaints.
+#  - Note this may yield a worse configuration,
+#     so be sure to only add sensible constraints
+# **********************************************
+
+# We force that one of the 15 most frequent alphabet characters are assigned to each of the 12 buttons.
+# for button in range(12):
+#     res = True
+#     for i in range(15):
+#         res = Or(res, G[i] == 2**button)
+#     s.add(res)
+#     print(res)
+
+s.add(Or(G[0] == 1, G[1] == 1, G[2] == 1, G[3] == 1, G[4] == 1, G[5] == 1, G[6] == 1, G[7] == 1, G[8] == 1, G[9] == 1, G[10] == 1, G[11] == 1, G[12] == 1, G[13] == 1, G[14] == 1))
+s.add(Or(G[0] == 2, G[1] == 2, G[2] == 2, G[3] == 2, G[4] == 2, G[5] == 2, G[6] == 2, G[7] == 2, G[8] == 2, G[9] == 2, G[10] == 2, G[11] == 2, G[12] == 2, G[13] == 2, G[14] == 2))
+s.add(Or(G[0] == 4, G[1] == 4, G[2] == 4, G[3] == 4, G[4] == 4, G[5] == 4, G[6] == 4, G[7] == 4, G[8] == 4, G[9] == 4, G[10] == 4, G[11] == 4, G[12] == 4, G[13] == 4, G[14] == 4))
+s.add(Or(G[0] == 8, G[1] == 8, G[2] == 8, G[3] == 8, G[4] == 8, G[5] == 8, G[6] == 8, G[7] == 8, G[8] == 8, G[9] == 8, G[10] == 8, G[11] == 8, G[12] == 8, G[13] == 8, G[14] == 8))
+s.add(Or(G[0] == 16, G[1] == 16, G[2] == 16, G[3] == 16, G[4] == 16, G[5] == 16, G[6] == 16, G[7] == 16, G[8] == 16, G[9] == 16, G[10] == 16, G[11] == 16, G[12] == 16, G[13] == 16, G[14] == 16))
+s.add(Or(G[0] == 32, G[1] == 32, G[2] == 32, G[3] == 32, G[4] == 32, G[5] == 32, G[6] == 32, G[7] == 32, G[8] == 32, G[9] == 32, G[10] == 32, G[11] == 32, G[12] == 32, G[13] == 32, G[14] == 32))
+s.add(Or(G[0] == 64, G[1] == 64, G[2] == 64, G[3] == 64, G[4] == 64, G[5] == 64, G[6] == 64, G[7] == 64, G[8] == 64, G[9] == 64, G[10] == 64, G[11] == 64, G[12] == 64, G[13] == 64, G[14] == 64))
+s.add(Or(G[0] == 128, G[1] == 128, G[2] == 128, G[3] == 128, G[4] == 128, G[5] == 128, G[6] == 128, G[7] == 128, G[8] == 128, G[9] == 128, G[10] == 128, G[11] == 128, G[12] == 128, G[13] == 128, G[14] == 128))
+s.add(Or(G[0] == 256, G[1] == 256, G[2] == 256, G[3] == 256, G[4] == 256, G[5] == 256, G[6] == 256, G[7] == 256, G[8] == 256, G[9] == 256, G[10] == 256, G[11] == 256, G[12] == 256, G[13] == 256, G[14] == 256))
+s.add(Or(G[0] == 512, G[1] == 512, G[2] == 512, G[3] == 512, G[4] == 512, G[5] == 512, G[6] == 512, G[7] == 512, G[8] == 512, G[9] == 512, G[10] == 512, G[11] == 512, G[12] == 512, G[13] == 512, G[14] == 512))
+s.add(Or(G[0] == 1024, G[1] == 1024, G[2] == 1024, G[3] == 1024, G[4] == 1024, G[5] == 1024, G[6] == 1024, G[7] == 1024, G[8] == 1024, G[9] == 1024, G[10] == 1024, G[11] == 1024, G[12] == 1024, G[13] == 1024, G[14] == 1024))
+s.add(Or(G[0] == 2048, G[1] == 2048, G[2] == 2048, G[3] == 2048, G[4] == 2048, G[5] == 2048, G[6] == 2048, G[7] == 2048, G[8] == 2048, G[9] == 2048, G[10] == 2048, G[11] == 2048, G[12] == 2048, G[13] == 2048, G[14] == 2048))
+
+# **********************************************
+# Cost constraints
 #  - Estimate and minimize cost of configuration 
 # **********************************************
 
@@ -137,28 +169,26 @@ for i in range(26, len(n_gram)):
 cost = [ Real('rc%s' % i) for i in range(len(n_gram)) ]
 null_n_gram_cost = [ Real('nc%s' % i) for i in range(len(n_gram)) ]
 s.add( [ cost[i] == \
-    If(And(Extract(11, 11, G[i]) == 1, Extract(10, 10, G[i]) == 1),  0.5, #  110 000 000 000
-    If(And(Extract(10, 10, G[i]) == 1, Extract(9 , 9 , G[i]) == 1),  0.5, #  011 000 000 000
-    If(And(Extract(8 , 8 , G[i]) == 1, Extract(7 , 7 , G[i]) == 1),  0.5, #  000 110 000 000
-    If(And(Extract(7 , 7 , G[i]) == 1, Extract(6 , 6 , G[i]) == 1),  0.5, #  000 011 000 000
-    If(And(Extract(5 , 5 , G[i]) == 1, Extract(4 , 4 , G[i]) == 1),  0.5, #  000 000 110 000
-    If(And(Extract(4 , 4 , G[i]) == 1, Extract(3 , 3 , G[i]) == 1),  0.5, #  000 000 011 000
-    If(And(Extract(2 , 2 , G[i]) == 1, Extract(1 , 1 , G[i]) == 1),  0.5, #  000 000 000 110
-    If(And(Extract(1 , 1 , G[i]) == 1, Extract(0 , 0 , G[i]) == 1),  0.5, #  000 000 000 011
-    If(Extract(0 , 0 , G[i]) == 1,  0.5, #  000 000 000 001
-    If(Extract(1 , 1 , G[i]) == 1,  0.5, #  000 000 000 010
-    If(Extract(2 , 2 , G[i]) == 1,  0.5, #  000 000 000 100
-    If(Extract(3 , 3 , G[i]) == 1,  0.5, #  000 000 001 000
-    If(Extract(4 , 4 , G[i]) == 1,  0.5, #  000 000 010 000
-    If(Extract(5 , 5 , G[i]) == 1,  0.5, #  000 000 100 000
-    If(Extract(6 , 6 , G[i]) == 1,  0.5, #  000 001 000 000
-    If(Extract(7 , 7 , G[i]) == 1,  0.5, #  000 010 000 000
-    If(Extract(8 , 8 , G[i]) == 1,  0.5, #  000 100 000 000
-    If(Extract(9 , 9 , G[i]) == 1,  0.5, #  001 000 000 000
-    If(Extract(10, 10, G[i]) == 1,  0.5, #  010 000 000 000
-    If(Extract(11, 11, G[i]) == 1,  0.5, #  100 000 000 000
-
-
+    If(And(Extract(11, 11, G[i]) == 1, Extract(10, 10, G[i]) == 1),  0.5 / len(n_gram[i]), #  110 000 000 000
+    If(And(Extract(10, 10, G[i]) == 1, Extract(9 , 9 , G[i]) == 1),  0.5 / len(n_gram[i]), #  011 000 000 000
+    If(And(Extract(8 , 8 , G[i]) == 1, Extract(7 , 7 , G[i]) == 1),  0.5 / len(n_gram[i]), #  000 110 000 000
+    If(And(Extract(7 , 7 , G[i]) == 1, Extract(6 , 6 , G[i]) == 1),  0.5 / len(n_gram[i]), #  000 011 000 000
+    If(And(Extract(5 , 5 , G[i]) == 1, Extract(4 , 4 , G[i]) == 1),  0.5 / len(n_gram[i]), #  000 000 110 000
+    If(And(Extract(4 , 4 , G[i]) == 1, Extract(3 , 3 , G[i]) == 1),  0.5 / len(n_gram[i]), #  000 000 011 000
+    If(And(Extract(2 , 2 , G[i]) == 1, Extract(1 , 1 , G[i]) == 1),  0.5 / len(n_gram[i]), #  000 000 000 110
+    If(And(Extract(1 , 1 , G[i]) == 1, Extract(0 , 0 , G[i]) == 1),  0.5 / len(n_gram[i]), #  000 000 000 011
+    If(Extract(0 , 0 , G[i]) == 1,  0.5 / len(n_gram[i]), #  000 000 000 001
+    If(Extract(1 , 1 , G[i]) == 1,  0.5 / len(n_gram[i]), #  000 000 000 010
+    If(Extract(2 , 2 , G[i]) == 1,  0.5 / len(n_gram[i]), #  000 000 000 100
+    If(Extract(3 , 3 , G[i]) == 1,  0.5 / len(n_gram[i]), #  000 000 001 000
+    If(Extract(4 , 4 , G[i]) == 1,  0.5 / len(n_gram[i]), #  000 000 010 000
+    If(Extract(5 , 5 , G[i]) == 1,  0.5 / len(n_gram[i]), #  000 000 100 000
+    If(Extract(6 , 6 , G[i]) == 1,  0.5 / len(n_gram[i]), #  000 001 000 000
+    If(Extract(7 , 7 , G[i]) == 1,  0.5 / len(n_gram[i]), #  000 010 000 000
+    If(Extract(8 , 8 , G[i]) == 1,  0.5 / len(n_gram[i]), #  000 100 000 000
+    If(Extract(9 , 9 , G[i]) == 1,  0.5 / len(n_gram[i]), #  001 000 000 000
+    If(Extract(10, 10, G[i]) == 1,  0.5 / len(n_gram[i]), #  010 000 000 000
+    If(Extract(11, 11, G[i]) == 1,  0.5 / len(n_gram[i]), #  100 000 000 000
     #  This can only be reached if the n-gram has a null assignment (is assigned no chord)
     #   We set it equal to null_n_gram_cost[i], null_n_gram_cost is just a placeholder to 
     #   prevent verbose code.
@@ -205,14 +235,15 @@ count = RealVal(total_count)
 chars_per_second = Real("cps")
 s.add(chars_per_second == total_count / cumulative_cost[len(n_gram)-1])
 
-
 # **************************************************
 # Sit back relax and let the SMT solver do the work.
 # **************************************************
 
-s.set("timeout", timeout)
-print(f"N-Grams: {str(len(n_gram))}, Setup Time: {datetime.now() - setupTime}")
-print("------------------------")
+# Timeout is given in milliseconds
+s.set("timeout", (timeout.days * 24 * 60 * 60 + timeout.seconds) * 1000)
+total_time = datetime.now() - setupTime
+print(f"N-Grams: {str(len(n_gram))}, Setup Time: {total_time}")
+print("---------------------------------------")
 
 hi_sat = 0
 lo_unsat = float("inf")
@@ -230,21 +261,28 @@ while min(lo_unsat, lo_unknown, cps_hi) - max(hi_sat, cps_lo) > cps_res:
     s.add(cumulative_cost[len(n_gram)-1] <= guess_max_cumulative_cost)
     
     result = s.check()
-    print(f"CPS: {guess_cps:.4f} - {result} - {datetime.now() - solveTime}")
+    guess_time = datetime.now() - solveTime
+    total_time += guess_time
+    print(f"CPS: {guess_cps:.4f} - {str(result):7} - {datetime.now() - solveTime}")
 
     if result == sat:
         hi_sat = guess_cps
         m = s.model()
     elif result == unsat:
         lo_unsat = guess_cps
+        s.pop() # Restore state (i.e. Remove guess constraint)
+                # Only remove guess constraint when it can't be attained, not when sat.
     elif result == unknown:
         lo_unknown = guess_cps
+        s.pop() # Restore state (i.e. Remove guess constraint)
+                # Only remove guess constraint when it can't be attained, not when sat.
 
-    s.pop() # Restore state (i.e. Remove guess constraint)
+    # s.pop() # Restore state (i.e. Remove guess constraint)
 
-print("------------------------")
-print(f"With cutoff {cutoff}")
+print("---------------------------------------")
 print(f"Sat: {hi_sat}, Unknown: {lo_unknown}, Unsat: {lo_unsat}")
+print(f"Total Time: {total_time}")
+print("---------------------------------------")
 
 
 # ******************************************************
