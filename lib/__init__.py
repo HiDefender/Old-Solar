306cbed02041564c70b73af00a5fba8dd6a3cf82
Jared Soundy <jared_soundy@gmail.com>
2020-12-15 10:10:32 -0600

    Removed dead code

--- lib/__init__.py
+++ lib/__init__.py
@@ -2,7 +2,6 @@ from .parameters import Parameters
 from .load import NGrams
 from .load import load_files
 from .load import create_dict
-from .stride import stride_constraint
 from .buttons import problem_def
 from .buttons import cost_mcc
 from .buttons import cost_scc
